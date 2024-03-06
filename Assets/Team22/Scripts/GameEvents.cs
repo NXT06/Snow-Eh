9765764cf375eedb99524ad5efbe1d181322fa8f
Sydney_Smith <smisydne@sheridancollege.ca>
2024-03-06 17:01:22 -0500

    - added end screens and crudely done behaviour

--- Assets/Team22/Scripts/GameEvents.cs
+++ Assets/Team22/Scripts/GameEvents.cs
@@ -6,18 +6,29 @@ using UnityEngine.InputSystem.iOS;
 
 public class GameEvents : MicrogameInputEvents
 {
-
     // get how many snowballs are on each side/get whos the winner
 
+    public GameObject screen1;
+    public GameObject screen2;
+
+    public float screenStopLocation = 9;
 
     protected override void OnTenSecondsLeft()
     {
-        // Code to execute when there are 10 seconds left in the game
+        // executes when there are 10 seconds left in the game, potential 10 second left message
     }
 
     protected override void OnTimesUp()
     {
-        //determine who is the winner, then call a prefab object with a behaviour script and proper end screen image, and have it drop down and maybe bounce
+        //determine who is the winner, then call EndScreenBehaviour and pass the winning screen
+
+        EndScreenBehaviour(screen1);
     }
 
+
+    public void EndScreenBehaviour(GameObject winningScreen)
+    {
+        // change screen location
+        winningScreen.transform.position = Vector3.zero;
+    }
 }
